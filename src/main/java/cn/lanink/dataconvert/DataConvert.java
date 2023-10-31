850fc02725c070a9e9772719429cd048b4a45bee
LT_Name <572413378@qq.com>
2023-10-31 20:57:49 +0800

    Stuff

--- src/main/java/cn/lanink/dataconvert/DataConvert.java
+++ src/main/java/cn/lanink/dataconvert/DataConvert.java
@@ -1,23 +1,25 @@
 package cn.lanink.dataconvert;
 
 import cn.lanink.dataconvert.convert.RuntimeBlockStateConvert;
+import lombok.extern.log4j.Log4j2;
 
 import java.io.IOException;
 
 /**
  * @author LT_Name
  */
+@Log4j2
 public class DataConvert {
 
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
-        System.out.println("start convert runtime_block_states");
+        log.info("Hello, World!");
+        log.info("start convert runtime_block_states");
         try {
             RuntimeBlockStateConvert.convert();
         } catch (IOException e) {
-            throw new RuntimeException(e);
+            log.error("convert runtime_block_states error", e);
         }
-        System.out.println("convert blockstate success");
+        log.info("convert blockstate success");
 
         //TODO creative_items
         System.exit(0);
--- src/main/java/cn/lanink/dataconvert/convert/RuntimeBlockStateConvert.java
+++ src/main/java/cn/lanink/dataconvert/convert/RuntimeBlockStateConvert.java
@@ -2,27 +2,44 @@ package cn.lanink.dataconvert.convert;
 
 import cn.lanink.dataconvert.utils.NBTIO1;
 import cn.nukkit.nbt.NBTIO;
-import cn.nukkit.nbt.tag.CompoundTag;
-import cn.nukkit.nbt.tag.ListTag;
-import cn.nukkit.nbt.tag.Tag;
+import cn.nukkit.nbt.tag.*;
 import com.google.common.base.Preconditions;
 import it.unimi.dsi.fastutil.ints.Int2ObjectMap;
 import it.unimi.dsi.fastutil.ints.Int2ObjectOpenHashMap;
+import lombok.extern.log4j.Log4j2;
+import org.cloudburstmc.blockstateupdater.BlockStateUpdaters;
+import org.cloudburstmc.nbt.NbtList;
+import org.cloudburstmc.nbt.NbtMap;
+import org.cloudburstmc.nbt.NbtMapBuilder;
+import org.cloudburstmc.nbt.NbtType;
 
 import java.io.*;
 import java.nio.ByteOrder;
 import java.util.*;
 import java.util.zip.GZIPInputStream;
 
+@Log4j2
 public class RuntimeBlockStateConvert {
     public static void convert() throws IOException {
-        //使用旧的NK数据作为基础
-        ListTag<CompoundTag> oldTag;
-        try (InputStream stream = new FileInputStream("src/main/resources/Nukkit_Data/runtime_block_states_618.dat")) { //TODO 有新的数据时记得更新
+        int oldBlockStatesVersion = 618;
+        int targetBlockStatesVersion = 622;
+
+        //使用PM1E的数据作为更新判断的基础（因为相比nkx比较全）
+        ListTag<CompoundTag> oldBaseListTag;
+        try (InputStream stream = new FileInputStream("src/main/resources/Nukkit_Data/pm1e_runtime_block_states_" + oldBlockStatesVersion + ".dat")) {
             //noinspection unchecked
-            oldTag = (ListTag<CompoundTag>) NBTIO.readTag(new BufferedInputStream(new GZIPInputStream(stream)), ByteOrder.BIG_ENDIAN, false);
+            oldBaseListTag = (ListTag<CompoundTag>) NBTIO.readTag(new BufferedInputStream(new GZIPInputStream(stream)), ByteOrder.BIG_ENDIAN, false);
         } catch (IOException e) {
-            throw new AssertionError("Unable to locate runtime_block_states_618.dat", e);
+            throw new AssertionError("Unable to locate pm1e_runtime_block_states_" + oldBlockStatesVersion + ".dat", e);
+        }
+
+        //加载NKX的数据，对于NKX存在的数据我们不需要重复生成
+        ListTag<CompoundTag> nkxTag;
+        try (InputStream stream = new FileInputStream("src/main/resources/Nukkit_Data/nkx_runtime_block_states_" + targetBlockStatesVersion + ".dat")) {
+            //noinspection unchecked
+            nkxTag = (ListTag<CompoundTag>) NBTIO.readTag(new BufferedInputStream(new GZIPInputStream(stream)), ByteOrder.BIG_ENDIAN, false);
+        } catch (IOException e) {
+            throw new AssertionError("Unable to locate nkx_runtime_block_states_" + targetBlockStatesVersion + ".dat", e);
         }
 
         Int2ObjectMap<String> blockIdToPersistenceName = new Int2ObjectOpenHashMap<>();
@@ -60,7 +77,8 @@ public class RuntimeBlockStateConvert {
                 while (bis.available() > 0) {
                     CompoundTag tag = NBTIO1.read(bis, ByteOrder.BIG_ENDIAN, true);
                     tag.putInt("runtimeId", runtimeId++);
-                    tag.putInt("id", persistenceNameToBlockId.getOrDefault(tag.getString("name").toLowerCase(), -1));
+                    String name = tag.getString("name").toLowerCase();
+                    tag.putInt("id", persistenceNameToBlockId.getOrDefault(name, -1));
                     tags.add(tag);
                 }
             }
@@ -68,13 +86,22 @@ public class RuntimeBlockStateConvert {
             throw new AssertionError(e);
         }
 
+        //更新方块数据到新版本
+        ListTag<CompoundTag> listTag = new ListTag<>();
+        for (CompoundTag compoundTag : oldBaseListTag.getAll()) {
+            listTag.add(nbtMap2CompoundTag(BlockStateUpdaters.updateBlockState(compoundTag2NbtMap(compoundTag), oldBlockStatesVersion)));
+        }
+        oldBaseListTag = listTag;
+
         ListTag<CompoundTag> newTagList = new ListTag<>();
+        newTagList.setAll(nkxTag.getAll());
 
         int data = 0;
         String lastBlockName = null;
         for (CompoundTag block : tags) {
             //根据名称获取旧的方块数据
-            List<CompoundTag> oldTagList = getBlockByName(oldTag, block.getString("name"));
+            String name = block.getString("name");
+            List<CompoundTag> oldTagList = getBlockByName(oldBaseListTag, name, null);
             if (oldTagList.isEmpty()) {
                 continue; //跳过未实现的方块
             }
@@ -91,43 +118,224 @@ public class RuntimeBlockStateConvert {
                     equalsTag = tag;
                     break;
                 } else {
+                    boolean equals = true;
                     for (Tag tag1 : newBlockStates) {
-                        if (oldBlockStates.contains(tag1.getName()) && oldBlockStates.get(tag1.getName()).equals(tag1)) {
-                            equalsTag = tag;
+                        if (!oldBlockStates.contains(tag1.getName())
+                                || !oldBlockStates.get(tag1.getName()).parseValue().equals(tag1.parseValue())) {
+                            equals = false;
                             break;
                         }
-
-                        /*Object object = tag1.parseValue();
-                        if (tag1 instanceof CompoundTag compoundTag && compoundTag.getTags().isEmpty()
-                                || tag1 instanceof ListTag<?> listTag && listTag.getAll().isEmpty()
-                                || object instanceof Number number && number.intValue() == 0
-                                || object instanceof String string && string.isBlank()
-                                || object instanceof Array array && Array.getLength(array) == 0) {
-
-                        }*/
+                    }
+                    if (equals) {
+                        equalsTag = tag;
                     }
                 }
             }
 
             if (equalsTag != null) {
                 CompoundTag copy = equalsTag.copy();
-                copy.putInt("runtimeId", block.getInt("runtimeId"));
+                int runtimeId = block.getInt("runtimeId");
+                copy.putInt("runtimeId", runtimeId);
                 copy.putInt("version", block.getInt("version"));
-                newTagList.add(copy);
+                boolean isDuplicate = false;
+                for (CompoundTag tag : newTagList.getAll()) {
+                    if (tag.getInt("runtimeId") == runtimeId
+                            || (tag.getString("name").equals(copy.getString("name"))
+                            && tag.getInt("id") == copy.getInt("id")
+                            && tag.getShort("data") == copy.getShort("data")
+                            && tag.getByte("stateOverload") == copy.getByte("stateOverload"))) {
+                        isDuplicate = true;
+                        log.debug("Duplicate : " + block + "\n" + copy + "\n " + tag);
+                    }
+                }
+                if (!isDuplicate) {
+                    newTagList.add(copy);
+                }
             }
         }
 
-        OutputStream outputStream = new BufferedOutputStream(new FileOutputStream("src/main/resources/Nukkit_Data/new_runtime_block_states.dat"));
+        OutputStream outputStream = new BufferedOutputStream(new FileOutputStream("src/main/resources/Target_Data/runtime_block_states_" + targetBlockStatesVersion + ".dat"));
         NBTIO1.writeGZIPCompressed(newTagList, outputStream, ByteOrder.BIG_ENDIAN);
     }
 
-    private static List<CompoundTag> getBlockByName(ListTag<CompoundTag> tags, String name) {
+    private static List<CompoundTag> getBlockByName(ListTag<CompoundTag> tags, String name, Integer meta) {
         ArrayList<CompoundTag> list = new ArrayList<>();
         for (CompoundTag block : tags.getAll()) {
-            if (block.getString("name").equals(name)) {
+            if (block.getString("name").equals(name)
+                    && (meta == null || block.getShort("data") == meta)) {
                 list.add(block);
             }
         }
         return list;
+    }
+
+    private static NbtMap compoundTag2NbtMap(CompoundTag compoundTag) {
+        NbtMapBuilder builder = NbtMap.builder();
+        for (Tag tag : compoundTag.getAllTags()) {
+            switch (tag.getId()) {
+                case 1:
+                    builder.putByte(tag.getName(), (byte) ((Integer) tag.parseValue()).intValue());
+                    break;
+                case 2:
+                    builder.putShort(tag.getName(), (short) ((Integer) tag.parseValue()).intValue());
+                    break;
+                case 3:
+                    builder.putInt(tag.getName(), (int) tag.parseValue());
+                    break;
+                case 4:
+                    builder.putLong(tag.getName(), (long) tag.parseValue());
+                    break;
+                case 5:
+                    builder.putFloat(tag.getName(), (float) tag.parseValue());
+                    break;
+                case 6:
+                    builder.putDouble(tag.getName(), (double) tag.parseValue());
+                    break;
+                case 7:
+                    builder.putByteArray(tag.getName(), (byte[]) tag.parseValue());
+                    break;
+                case 8:
+                    builder.putString(tag.getName(), (String) tag.parseValue());
+                    break;
+                case 9:
+                    ListTag listTag = (ListTag) tag;
+                    builder.putList(tag.getName(), tagType2NbtType(listTag.type), listTag2NbtList(listTag));
+                    break;
+                case 10:
+                    builder.putCompound(tag.getName(), compoundTag2NbtMap((CompoundTag) tag));
+                    break;
+                case 11:
+                    builder.putIntArray(tag.getName(), (int[]) tag.parseValue());
+                    break;
+            }
+        }
+        return builder.build();
+    }
+
+    private static CompoundTag nbtMap2CompoundTag(NbtMap nbtMap) {
+        CompoundTag compoundTag = new CompoundTag();
+        for (String key : nbtMap.keySet()) {
+            Object value = nbtMap.get(key);
+            if (value instanceof Byte) {
+                compoundTag.putByte(key, (byte) value);
+            } else if (value instanceof Short) {
+                compoundTag.putShort(key, (short) value);
+            } else if (value instanceof Integer) {
+                compoundTag.putInt(key, (int) value);
+            } else if (value instanceof Long) {
+                compoundTag.putLong(key, (long) value);
+            } else if (value instanceof Float) {
+                compoundTag.putFloat(key, (float) value);
+            } else if (value instanceof Double) {
+                compoundTag.putDouble(key, (double) value);
+            } else if (value instanceof byte[]) {
+                compoundTag.putByteArray(key, (byte[]) value);
+            } else if (value instanceof String) {
+                compoundTag.putString(key, (String) value);
+            } else if (value instanceof NbtList nbtList) {
+                compoundTag.put(key, nbtList2ListTag(nbtList));
+            } else if (value instanceof NbtMap) {
+                compoundTag.putCompound(key, nbtMap2CompoundTag((NbtMap) value));
+            } else if (value instanceof int[]) {
+                compoundTag.putIntArray(key, (int[]) value);
+            }
+        }
+        return compoundTag;
+    }
+
+    private static NbtList listTag2NbtList(ListTag<Tag> listTag) {
+        NbtList nbtList = new NbtList(tagType2NbtType(listTag.type));
+        for (Tag tag : listTag.getAll()) {
+            if (tag instanceof ByteTag) {
+                nbtList.add(((ByteTag) tag).parseValue());
+            } else if (tag instanceof ShortTag) {
+                nbtList.add(((ShortTag) tag).parseValue());
+            } else if (tag instanceof IntTag) {
+                nbtList.add(((IntTag) tag).parseValue());
+            } else if (tag instanceof LongTag) {
+                nbtList.add(((LongTag) tag).parseValue());
+            } else if (tag instanceof FloatTag) {
+                nbtList.add(((FloatTag) tag).parseValue());
+            } else if (tag instanceof DoubleTag) {
+                nbtList.add(((DoubleTag) tag).parseValue());
+            } else if (tag instanceof ByteArrayTag) {
+                nbtList.add(((ByteArrayTag) tag).parseValue());
+            } else if (tag instanceof StringTag) {
+                nbtList.add(((StringTag) tag).parseValue());
+            } else if (tag instanceof ListTag listTag1) {
+                nbtList.add(listTag2NbtList(listTag1));
+            } else if (tag instanceof CompoundTag compoundTag) {
+                nbtList.add(compoundTag2NbtMap(compoundTag));
+            } else if (tag instanceof IntArrayTag) {
+                nbtList.add(((IntArrayTag) tag).parseValue());
+            }
+        }
+        return nbtList;
+    }
+
+    private static ListTag<Tag> nbtList2ListTag(NbtList nbtList) {
+        ListTag<Tag> listTag = new ListTag<>();
+        for (Object value : nbtList) {
+            if (value instanceof Byte) {
+                listTag.add(new ByteTag("", (byte) value));
+            } else if (value instanceof Short) {
+                listTag.add(new ShortTag("", (short) value));
+            } else if (value instanceof Integer) {
+                listTag.add(new IntTag("", (int) value));
+            } else if (value instanceof Long) {
+                listTag.add(new LongTag("", (long) value));
+            } else if (value instanceof Float) {
+                listTag.add(new FloatTag("", (float) value));
+            } else if (value instanceof Double) {
+                listTag.add(new DoubleTag("", (double) value));
+            } else if (value instanceof byte[]) {
+                listTag.add(new ByteArrayTag("", (byte[]) value));
+            } else if (value instanceof String) {
+                listTag.add(new StringTag("", (String) value));
+            } else if (value instanceof NbtList nbtList1) {
+                listTag.add(nbtList2ListTag(nbtList1));
+            } else if (value instanceof NbtMap) {
+                listTag.add(nbtMap2CompoundTag((NbtMap) value));
+            } else if (value instanceof int[]) {
+                listTag.add(new IntArrayTag("", (int[]) value));
+            }
+        }
+        return listTag;
+    }
+
+    private static NbtType<?> tagType2NbtType(int tagType) {
+        return switch (tagType) {
+            case 0 -> NbtType.END;
+            case 1 -> NbtType.BYTE;
+            case 2 -> NbtType.SHORT;
+            case 3 -> NbtType.INT;
+            case 4 -> NbtType.LONG;
+            case 5 -> NbtType.FLOAT;
+            case 6 -> NbtType.DOUBLE;
+            case 7 -> NbtType.BYTE_ARRAY;
+            case 8 -> NbtType.STRING;
+            case 9 -> NbtType.LIST;
+            case 10 -> NbtType.COMPOUND;
+            case 11 -> NbtType.INT_ARRAY;
+            default -> throw new IllegalArgumentException("Unknown tag type " + tagType);
+        };
+    }
+
+    private static int nbtType2TagType(NbtType<?> nbtType) {
+        return switch (nbtType.getEnum()) {
+            case END -> 0;
+            case BYTE -> 1;
+            case SHORT -> 2;
+            case INT -> 3;
+            case LONG -> 4;
+            case FLOAT -> 5;
+            case DOUBLE -> 6;
+            case BYTE_ARRAY -> 7;
+            case STRING -> 8;
+            case LIST -> 9;
+            case COMPOUND -> 10;
+            case INT_ARRAY -> 11;
+            default -> throw new IllegalArgumentException("Unknown tag type " + nbtType);
+        };
     }
 }
